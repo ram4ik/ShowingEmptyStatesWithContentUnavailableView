7b98144227d409e52bd77998067d30ee1a247857
Ramill Ibragimov <ribragimov@malwarebytes.com>
2023-12-10 15:35:30 +0200

    Initial Commit

---
+++ ShowingEmptyStatesWithContentUnavailableView/ShowingEmptyStatesWithContentUnavailableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowingEmptyStatesWithContentUnavailableViewApp.swift
+//  ShowingEmptyStatesWithContentUnavailableView
+//
+//  Created by test on 10.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShowingEmptyStatesWithContentUnavailableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
