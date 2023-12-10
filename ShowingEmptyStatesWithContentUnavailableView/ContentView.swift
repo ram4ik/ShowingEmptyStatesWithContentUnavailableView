b4398bf82a1c8b12c3b101e9c90fbc2b5247d03c
Ramill Ibragimov <ribragimov@malwarebytes.com>
2023-12-10 15:43:05 +0200

    ContentUnavailable

--- ShowingEmptyStatesWithContentUnavailableView/ContentView.swift
+++ ShowingEmptyStatesWithContentUnavailableView/ContentView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ContentUnavailableView("No snipets", systemImage: "swift", description: Text("You don't have any saved snippets"))
     }
 }
 
